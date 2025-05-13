060641314d696ede2163a8c9e03448a7e71b3e37
juancarlos-197 <juancarlos-197@unimayor.edu.co>
2025-05-13 11:17:42 -0500

    Variables

---
+++ Basic/01-variables.js
@@ -0,0 +1,33 @@
+/* Un video en javaScripy
+Un archivo que se llama Variables.
+Imprimir cosas por consola, pero tamboco tiene mucha uticidad.
+Programación son las variables, es decir la posicilidad  de que pueda almacenar en memoria valores 
+y referencias y despues trabajar con ellos.
+*/
+
+
+// var (palabra recervada) Oreferencia a un objeto en javaScript
+
+var helloWorld = " Hello, javaScript";
+console.log(helloWorld);
+
+helloWorld = " Hello, javaScript";
+console.log(helloWorld);
+
+
+// let solo son accesibles dentro de un bloque 
+
+let helloWorld2 = " Hello 2, javaScript2";
+console.log(helloWorld2);
+
+helloWorld2 = " Hello 2, javaScript2";
+console.log(helloWorld2);
+
+
+
+// const
+const helloWorld3 = " Hello 3, javaScript3";
+console.log(helloWorld3);
+
+helloWorld3 = " Hello 3, javaScript3";
+console.log(helloWorld3);
