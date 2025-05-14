57c1aa9cad56ea8a4bd5607fa9301d6b7d79f866
juancarlos-197 <juancarlos-197@unimayor.edu.co>
2025-05-13 22:37:46 -0500

    Operadores

---
+++ Basic/03-operadores.js
@@ -0,0 +1,23 @@
+// operadores  se utilizan para relacionas esos datos , para operar eso datos 
+// realizar operaciones
+
+//operaciones aritmeticas
+
+let a =5
+let b =15
+
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+
+console.log(a % b)//Modulo
+console.log(a ** b)//Exponencial
+
+a++
+console.log(a)//Incremento
+
+b--
+console.log(b)//Decremento
+
+
