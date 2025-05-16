f1d4f7f050077670e0c9c5956aa470a3b6ac572c
juancarlos-197 <juancarlos-197@unimayor.edu.co>
2025-05-15 21:56:44 -0500

    Array

---
+++ Basic/04-arrays.js
@@ -0,0 +1,10 @@
+let animales = ['perro','gato','pollo']
+console.log(animales)
+console.log(animales[2])
+animales[6]='rana'
+console.log(animales)
+
+console.log(animales[5])
+console.log(typeof animales)
+
+console.log(animales.length)
